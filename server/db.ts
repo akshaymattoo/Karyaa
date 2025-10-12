b12610430796c948728a7c90db250fdc88b66de2
Akshay Mattoo <akshaymattoo@gmail.com>
2025-10-12 18:29:31 +0000

    addd db changes
    addd db changes

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,26 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import * as schema from '@shared/schema';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import WebSocket from 'ws';
+// import { Pool, neonConfig } from '@neondatabase/serverless';
+// import * as schema from '@shared/schema';
+// import { drizzle } from 'drizzle-orm/neon-serverless';
+// import WebSocket from 'ws';
 
-// Neon uses WebSockets under the hood; provide the Node WebSocket implementation.
-neonConfig.webSocketConstructor = WebSocket;
+// // Neon uses WebSockets under the hood; provide the Node WebSocket implementation.
+// neonConfig.webSocketConstructor = WebSocket;
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 
-export const db = drizzle(pool, { schema });
+// export const db = drizzle(pool, { schema });
+
+
+import 'dotenv/config';
+
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+
+const connectionString = process.env.DATABASE_URL || "";
+
+// Disable prefetch as it is not supported for "Transaction" pool mode
+export const client = postgres(connectionString, { prepare: false })
+export const db = drizzle(client);
+
+
